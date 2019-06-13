bf1258555eec2cace55d096127e8f7ea95f1054b
hanlonglin <1501295534@qq.com>
2019-06-13 10:07:24 +0800

    基准项目模板

---
+++ app/src/main/java/com/dragonforest/android/templateandroidapplication/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.dragonforest.android.templateandroidapplication.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dragonforest.android.templateandroidapplication.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
